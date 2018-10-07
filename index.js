b4d4de5c322f2f0746b271abc4dc962260f97761
functional-domain-3580 <saranderson13@gmail.com>
2018-10-07 02:12:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+if(name === "Susan") {
+  console.log("I'm not Joe, I'm Susan")
+} else {
+  console.log("Who tf is Susan?")
+}
+
 
 // Don't worry about this
 module.exports = { name, height
